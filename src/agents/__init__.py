6a7db7337d145cf4bd0445ef4ef6fe0b716c34c4
vodaiphuoc <phuocvodn98gmail.com>
2024-10-01 12:13:34 +0100

    edited format

---
+++ src/agents/__init__.py
@@ -0,0 +1,2 @@
+"""This module contains example of agents that can be used to interact with the environment."""
+from sumo_rl_repo.agents.ql_agent import QLAgent
---
+++ src/exploration/__init__.py
@@ -0,0 +1,3 @@
+"""Module for exploration strategies."""
+
+from sumo_rl_repo.exploration.epsilon_greedy import EpsilonGreedy
