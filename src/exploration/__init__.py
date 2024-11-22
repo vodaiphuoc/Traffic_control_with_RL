ab92cf00ab464628585289f3c3a104609b5c81fe
vodaiphuoc <phuocvodn98gmail.com>
2024-11-22 12:45:02 +0000

    test

--- src/exploration/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Module for exploration strategies."""
-
-from sumo_rl_repo.exploration.epsilon_greedy import EpsilonGreedy
--- src/memory/memories_v2.py
+++ src/memory/memories_v2.py
@@ -8,32 +8,68 @@ from collections import namedtuple
 Base_Transition = namedtuple('Base_Transition',['state','action'])
 
 class Transition(Base_Transition):
+	"""Class represents single transition or a batch of transition"""
 	def __new__(cls,
 				state: Union[TensorDict, Tuple[TensorDict]],
-				action: Union[torch.Tensor, Tuple[torch.Tensor]]
+				action: Union[torch.Tensor, Tuple[torch.Tensor]],
+				device: torch.device = torch.device('cpu')
 				)->Base_Transition:
 		if isinstance(state, tuple) and isinstance(action, tuple):
 			return super(Transition,cls).__new__(cls, 
-												state = torch.cat(state), 
-												action = torch.cat(action))
+												state = torch.cat(state).to(device), 
+												action = torch.cat(action).to(device))
 		
 		elif isinstance(state, TensorDict) and isinstance(action, torch.Tensor):
-			return super(Transition,cls).__new__(cls, 
-												state = state, 
-												action = action)
+			return super(Transition,cls).__new__(cls,
+												state = state.to(device), 
+												action = action.to(device))
 		else:
 			raise NotImplementedError(f"Not NotImplementedError for types: {type(state)} and {type(action)}")
 
 	@property
-	def _is_batch(self)->bool:
+	def is_batch(self)->bool:
 		return self.state.shape[0] > 1
 
 	def __len__(self):
 		return self.state.shape[0]
 
-# class ReplayMemory(object):
-#     def __init__(self, capacity:int):
-#         self.memory = deque([], maxlen=capacity)
-#         self.capacity = capacity
+class ReplayMemory(Transition):
+	"""
+	Buffer relay memory which manipulate Transition
+	ref: https://pytorch.org/tensordict/stable/tutorials/tensordict_preallocation.html
+	"""
+	def __new__(cls, 
+				capacity:int, 
+				device: torch.device = torch.device('cuda'),
+				priority_sampling: bool = False,
+				)->Transition:
+		return super(ReplayMemory,cls).__new__(cls,
+												state = TensorDict({}, batch_size = capacity),
+												action = torch.empty(size = [capacity], dtype = torch.int8),
+												device = device
+												)
+	def __init__(self,
+				capacity:int,
+				device: torch.device = torch.device('cuda'),
+				priority_sampling: bool = False,
+				)->None:
+		self.current_index = -1
+		self.capacity = capacity
 
-#     
+	@property
+	def is_full(self)->bool:
+		return self.current_index+1 == self.capacity
+
+	# def _random_sampling():
+
+
+
+	def push(self, transition: Transition, index:int = None):
+		
+		
+
+		self.current_index += 1
+
+		self.state[self.current_index] = transition.state[0]
+		self.action[self.current_index] = transition.action
+
